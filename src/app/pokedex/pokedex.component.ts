e313c649d4d5f80a1584fa09efd974c8a9375741
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-25 10:23:24 -0300

    3

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokeF } from './PokeF';
 
 @Component({
   selector: 'app-pokedex',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokedex.component.css']
 })
 export class PokedexComponent {
+  id: number  = 1;
+  img: string = "";
+  
+  pokemonF : PokeF = {
+    "imagem" : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+  } as PokeF;
 
+  proximo(){
+    this.id++;
+    if(this.id < 10){
+      this.img = "00"+this.id;
+    }else if(this.id < 100){
+      this.img = "0"+this.id;
+    }else{
+      this.img = this.id.toString();
+    }
+    this.pokemonF.imagem="https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+this.img+".png"
+  }
+
+  anterior(){
+    this.id--;
+    if(this.id < 1){
+      this.id = 1;
+      this.img = "00"+this.id;
+    }else if(this.id < 10){
+      this.img = "00"+this.id;
+    }else if(this.id < 100){
+      this.img = "0"+this.id;
+    }else if(this.id < 1009){
+      this.img = this.id.toString();
+    }else{
+      this.id = 1;
+      this.img = "00"+this.id;
+    }
+    this.pokemonF.imagem="https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+this.img+".png"
+  }
 }
